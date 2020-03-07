e660377d11533e0a179bb43bd19acbb7d6a224b2
sunli <sl17325309332@163.com>
2020-03-07 13:05:09 +0800

    add getCity

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,22 +1,25 @@
 <template>
     <div class="cinema_body">
-        <ul>
-            <li v-for="item in cinemaList" :key="item.id">
-                <div>
-                    <span>{{item.nm}}</span>
-                    <span class="q"><span class="price">{{item.sellPrice}}</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>{{item.addr}}</span>
-                    <span>{{item.distance}}</span>
-                </div>
-                <div class="card">
-                    <div v-for="(num,key) in item.tag" :key="key" v-if="num === 1" :class="key | classCard">
-                        {{key | formatCard}}
+        <Loading v-if="isLoading"></Loading>
+        <Scroller v-else>
+            <ul>
+                <li v-for="item in cinemaList" :key="item.id">
+                    <div>
+                        <span>{{item.nm}}</span>
+                        <span class="q"><span class="price">{{item.sellPrice}}</span> 元起</span>
                     </div>
-                </div>
-            </li>
-        </ul>
+                    <div class="address">
+                        <span>{{item.addr}}</span>
+                        <span>{{item.distance}}</span>
+                    </div>
+                    <div class="card">
+                        <div v-for="(num,key) in item.tag" :key="key" v-if="num === 1" :class="key | classCard">
+                            {{key | formatCard}}
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </Scroller>
     </div>
 </template>
 
@@ -25,15 +28,24 @@
         name: "CiList",
         data(){
             return {
-                cinemaList:[]
+                cinemaList : [],
+                isLoading : true,
+                prevCityId : -1
             }
         },
-        mounted() {
-            this.axios.get('/api/cinemaList?cityId=10')
+        activated() {
+            var cityId = this.$store.state.city.id
+            if (this.prevCityId === cityId){
+                return;
+            }
+            this.isLoading = true;
+            this.axios.get('/api/cinemaList?cityId='+cityId)
                 .then((res)=>{
                     var msg = res.data.msg;
                     if (msg === 'ok'){
+                        this.isLoading = false
                         this.cinemaList = res.data.data.cinemas;
+                        this.prevCityId = cityId;
                     }
                 })
         },
--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -1,50 +1,78 @@
 <template>
-    <div class="movie_body">
-        <ul>
-<!--            <li>-->
-<!--                <div class="pic_show"><img src="/images/movie_1.jpg"></div>-->
-<!--                <div class="info_list">-->
-<!--                    <h2>无名之辈</h2>-->
-<!--                    <p>观众评 <span class="grade">9.2</span></p>-->
-<!--                    <p>主演: 陈建斌,任素汐,潘斌龙</p>-->
-<!--                    <p>今天55家影院放映607场</p>-->
-<!--                </div>-->
-<!--                <div class="btn_mall">-->
-<!--                    购票-->
-<!--                </div>-->
-<!--            </li>-->
-            <li v-for="item in movieList" :key="item.id">
-                <div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
-                <div class="info_list">
-                    <h2>{{item.nm}}</h2>
-                    <p>观众评 <span class="grade">{{item.sc}}</span></p>
-                    <p>主演: {{item.star}}</p>
-                    <p>{{item.showInfo}}</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-        </ul>
+    <div class="movie_body" ref="movie_body">
+        <loading v-if="isLoading"></loading>
+        <Scroller v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd">
+            <ul>
+                <li class="pullDown">{{pullDownMsg}}</li>
+                <li v-for="item in movieList" :key="item.id">
+                    <div class="pic_show" @tap="handleToDetail"><img :src="item.img | setWH('128.180')"></div>
+                    <div class="info_list">
+                        <h2>{{item.nm}}</h2>
+                        <p>观众评 <span class="grade">{{item.sc}}</span></p>
+                        <p>主演: {{item.star}}</p>
+                        <p>{{item.showInfo}}</p>
+                    </div>
+                    <div class="btn_mall">
+                        购票
+                    </div>
+                </li>
+            </ul>
+        </Scroller>
     </div>
 </template>
 
 <script>
+    //
     export default {
         name: "NowPlaying",
         data(){
             return {
-                movieList:[]
+                movieList : [],
+                pullDownMsg : '',
+                isLoading : true,
+                prevCityId : -1
             }
         },
-        mounted() {
-            this.axios.get('/api/movieOnInfoList?cityId=10')
+        activated() {
+
+            var cityId = this.$store.state.city.id
+            if (this.prevCityId === cityId){
+                return;
+            }
+            this.isLoading = true;
+            this.axios.get('/api/movieOnInfoList?cityId='+cityId)
                 .then((res)=>{
                     var msg = res.data.msg;
                     if (msg === 'ok'){
                         this.movieList = res.data.data.movieList;
+                        this.isLoading = false
+                        this.prevCityId = cityId;
                     }
                 })
+        },
+        methods: {
+            handleToDetail() {
+                console.log('1111')
+            },
+            handleToScroll(pos) {
+                if (pos.y > 30) {
+                    this.pullDownMsg = '正在更新中';
+                }
+            },
+            handleToTouchEnd(pos){
+                if (pos.y > 30) {
+                    this.axios.get('/api/movieOnInfoList?cityId=10')
+                        .then((res)=>{
+                            if (msg === 'ok') {
+                                this.pullDownMsg = '更新成功'
+                                setTimeout(()=>{
+                                    this.movieList = res.data.data.movieList;
+                                    this.pullDownMsg = '';
+                                },1000)
+                            }
+                        });
+                }
+            }
         }
     }
 </script>
@@ -62,4 +90,5 @@
     .movie_body .info_list img{ width:50px; position: absolute; right:10px; top: 5px;}
     .movie_body .btn_mall , .movie_body .btn_pre{ width:47px; height:27px; line-height: 28px; text-align: center; background-color: #f03d37; color: #fff; border-radius: 4px; font-size: 12px; cursor: pointer;}
     .movie_body .btn_pre{ background-color: #3c9fe6;}
+    .movie_body .pullDown{margin: 0;padding: 0;border: 0;}
 </style>
